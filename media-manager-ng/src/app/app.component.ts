a8cf93ac2a370cc4a196b92d2ddf7b6467a5182c
Christopher Cortes <christophercr@gmail.com>
2024-09-26 14:31:27 +0200

    sesion-3: implementación de directiva estructural IfViewport
    - instalar librería `@angular/cdk`
    - inyectar servicio BreakpointObserver y observar cambios
    - usar ViewContainer y TemplateRef

--- media-manager-ng/src/app/app.component.ts
+++ media-manager-ng/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewMediaCollectionComponent } from './components/new-media-collection/new-media-collection.component';
 import { CustomButtonDirective } from './directives/custom-button.directive';
+import { IfViewportDirective } from './directives/if-viewport.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewMediaCollectionComponent, CustomButtonDirective],
+  imports: [RouterOutlet, NewMediaCollectionComponent, CustomButtonDirective, IfViewportDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ media-manager-ng/src/app/directives/if-viewport.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IfViewportDirective } from './if-viewport.directive';
+
+describe('IfViewportDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IfViewportDirective();
+    expect(directive).toBeTruthy();
+  });
+});
