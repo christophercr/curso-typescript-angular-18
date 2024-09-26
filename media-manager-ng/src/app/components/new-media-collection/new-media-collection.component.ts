e2b165c14c0f32a370c205c35a85c0a013496ddb
Christopher Cortes <christophercr@gmail.com>
2024-09-26 12:20:14 +0200

    sesion-3: refactor del componente NewMediaCollection para usar NgModel
    - two-way binding (banana in a box)
    - change detection Default vs OnPush

--- media-manager-ng/src/app/components/new-media-collection/new-media-collection.component.ts
+++ media-manager-ng/src/app/components/new-media-collection/new-media-collection.component.ts
@@ -1,42 +1,69 @@
 import type { OnChanges, SimpleChanges } from '@angular/core';
-import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, inject, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-media-collection',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-media-collection.component.html',
   styleUrl: './new-media-collection.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewMediaCollectionComponent implements OnInit, OnDestroy, AfterViewInit, OnChanges {
   @Input()
-  public name = 'default value';
+  public inputName: string[] = ['default value'];
 
   @Output()
   public reloadClicked = new EventEmitter<string>();
 
+  /*@ViewChild('nameInput')
+  public nameInput: ElementRef<HTMLInputElement> | null = null;*/
+
+  private _cdRef = inject(ChangeDetectorRef);
+
   public ngOnInit() {
-    console.log('ngOnInit called!');
+    //console.log('ngOnInit called!');
   }
 
   public ngAfterViewInit(): void {
-    console.log('ngAfterViewInit called!');
+    //console.log('ngAfterViewInit called!');
+
+    /*if (this.nameInput) {
+      this.nameInput.nativeElement.value = this.name;
+    }*/
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
-     console.log('ngOnChanges called!', changes);
- }
+    //console.log('ngOnChanges called!', changes);
+  }
 
   public ngOnDestroy() {
-    console.log('ngOnDestroy called!');
+    //console.log('ngOnDestroy called!');
   }
 
   public createBookCollection() {
-    console.log('Creating a new book collection...');
+    //console.log('Creating a new book collection...');
   }
 
   public reloadBookCollections() {
     console.log('Reloading book collections...');
     this.reloadClicked.emit('some data');
+    this._resetInput();
   }
+
+  /*public onNameChange(newValue: string) {
+    this.inputName = newValue;
+    console.log('Name changed:', this.inputName);
+  }*/
+   
+    private _resetInput() {
+      console.log('Resetting the form...');
+      setTimeout(() => {
+        console.log('X secs delay...');
+        this.inputName = ['default value'];
+        this._cdRef.markForCheck();
+      }, 2000);
+     }
+     
 }
